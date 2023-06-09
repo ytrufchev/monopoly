3d48c984d806736cae625f5e02901ef516f90ac5
ytrufchev <yordan.tihomirov@gmail.com>
2023-06-09 23:34:34 +0300

    adding dice and board.

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,112 @@
 public class Board {
+    private int tileNumber;
+    private String name;
+    private int price;
+    private int rent;
+    private int mortgageValue;
+    private String owner;
+    private boolean mortgaged;
+
+    private int type;
+
+    public Board(String name, int price, int rent, int mortgageValue, String owner, boolean mortgaged, int type) {
+        this.tileNumber = tileNumber;
+        this.name = name;
+        this.price = price;
+        this.rent = rent;
+        this.mortgageValue = mortgageValue;
+        this.owner = null; // Initially, no owner
+        this.mortgaged = false; // Initially, not mortgaged
+        this.type = type;
+    }
+
+    public int getTileNumber() {
+        return tileNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public int getMortgageValue() {
+        return mortgageValue;
+    }
+
+    public String getOwner() {
+        if(this.owner != null) {
+            return this.owner;
+        }
+        return "";
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public boolean isMortgaged() {
+        return mortgaged;
+    }
+    public int getType() {
+        return type;
+    }
+
+    public void setMortgaged(boolean mortgaged) {
+        this.mortgaged = mortgaged;
+    }
+
+    public static Board[] initializeTiles() {
+        Board[] tiles = new Board[40];
+
+        tiles[0] = new Board("Go", 0, 0, 0, "", false, 3);
+        tiles[1] = new Board("Mediterranean Avenue", 60, 50, 30, "", false, 0);
+        tiles[2] = new Board("Community Chest", 0, 0, 0, "", false, 4);
+        tiles[3] = new Board("Baltic Avenue", 60, 50, 30, "", false, 0);
+        tiles[4] = new Board("Income Tax", 0, 0, 0, "", false,2);
+        tiles[5] = new Board("Reading Railroad", 200, 120, 100, "", false, 0);
+        tiles[6] = new Board("Oriental Avenue", 100, 80, 50, "", false, 0);
+        tiles[7] = new Board("Chance", 0, 0, 0, "", false, 4);
+        tiles[8] = new Board("Vermont Avenue", 100, 70, 50, "", false, 0);
+        tiles[9] = new Board("Connecticut Avenue", 120, 85, 60, "", false, 0);
+        tiles[10] = new Board("Jail/Just Visiting", 0, 0, 0, "", false, 3);
+        tiles[11] = new Board("St. Charles Place", 140, 70, 70, "", false, 0);
+        tiles[12] = new Board("Electric Company", 150, 0, 0, "", false, 0);
+        tiles[13] = new Board("States Avenue", 140, 70, 70, "", false, 0);
+        tiles[14] = new Board("Virginia Avenue", 160, 90, 80, "", false, 0);
+        tiles[15] = new Board("Pennsylvania Railroad", 200, 120, 100, "", false, 0);
+        tiles[16] = new Board("St. James Place", 180, 100, 90, "", false, 0);
+        tiles[17] = new Board("Community Chest", 0, 0, 0, "", false, 4);
+        tiles[18] = new Board("Tennessee Avenue", 180, 100, 90, "", false, 0);
+        tiles[19] = new Board("New York Avenue", 200, 120, 100, "", false, 0);
+        tiles[20] = new Board("Free Parking", 0, 0, 0, "", false, 3);
+        tiles[21] = new Board("Kentucky Avenue", 220, 130, 110, "", false, 0);
+        tiles[22] = new Board("Chance", 0, 0, 0, "", false, 4);
+        tiles[23] = new Board("Indiana Avenue", 220, 130, 110, "", false, 0);
+        tiles[24] = new Board("Illinois Avenue", 240, 150, 120, "", false, 0);
+        tiles[25] = new Board("B. & O. Railroad", 200, 120, 100, "", false, 0);
+        tiles[26] = new Board("Atlantic Avenue", 260, 160, 130, "", false, 0);
+        tiles[27] = new Board("Ventnor Avenue", 260, 160, 130, "", false, 0);
+        tiles[28] = new Board("Water Works", 150, 100, 0, "", false, 0);
+        tiles[29] = new Board("Marvin Gardens", 280, 170, 140, "", false, 0);
+        tiles[30] = new Board("Go To Jail", 0, 0, 0, "", false, 1);
+        tiles[31] = new Board("Pacific Avenue", 300, 200, 150, "", false, 0);
+        tiles[32] = new Board("North Carolina Avenue", 300, 200, 150, "", false, 0);
+        tiles[33] = new Board("Community Chest", 0, 0, 0, "", false, 4);
+        tiles[34] = new Board("Pennsylvania Avenue", 320, 210, 160, "", false, 0);
+        tiles[35] = new Board("Short Line", 200, 25, 139, "", false, 0);
+        tiles[36] = new Board("Chance", 0, 0, 0, "", false, 4);
+        tiles[37] = new Board("Park Place", 350, 250, 175, "", false, 0);
+        tiles[38] = new Board("Luxury Tax", 0, 0, 0, "", false, 2);
+        tiles[39] = new Board("Boardwalk", 400, 300, 200, "", false, 0);
+
+
+        return tiles;
+    }
 }
--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,15 @@
+import java.util.Random;
+
 public class Dice {
-    
+    public int roll() {
+        Random random = new Random();
+        return random.nextInt(6) + 1; // Generates a random number between 1 and 6 (inclusive)
+    }
+
+    public int[] rollTwoDice() {
+        int[] rolls = new int[2];
+        rolls[0] = roll();
+        rolls[1] = roll();
+        return rolls;
+    }
 }
